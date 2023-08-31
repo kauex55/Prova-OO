80607a3ff54be68525eb20dc953babfde6481385
kauex55 <kauelima2477@gmail.com>
2023-08-31 09:21:31 -0300

    update final

--- classes.py
+++ classes.py
@@ -1,13 +1,9 @@
 class ToDoList:
-    def adicionar_tarefa(self, descricao, id):
-        self.id = id
+    def adicionar_tarefa(self, descricao):
         self.descricao = descricao
-        self.a = {}
-        self.a[self.id] = [self.descricao]
-
+ 
     def excluir_tarefa(self, indice):
-        self.indice = indice
+        del self
 
     def listar_tarefas(self):
-        for chave,a in self.reserva.items():
-            print(f"ID:{chave} - Tarefas:{a}")
+        print(self.descricao)
